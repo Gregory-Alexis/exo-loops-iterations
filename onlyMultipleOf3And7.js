06711658a6e454ae31de1d0b855c63c207318c01
Westindiess <gregoryalexiss@gmail.com>
2021-02-27 16:41:46 +0100

    Exo loops intérations

--- onlyMultipleOf3And7.js
+++ onlyMultipleOf3And7.js
@@ -1,12 +1,12 @@
 for (let i = 1; i <= 1000; i++) {
-  if (i % 3 && i % 7) {
-
+  if (i % 3 === 0 && i % 7 === 0) {
+    console.log(i + ': ' + ' Je suis une multiple de 3 et de 7')
   } else {
     if (i % 3 === 0) {
-      console.log(i);
+      console.log(i + ': ' + ' Je suis un multiple de 3');
     }
     if (i % 7 === 0) {
-      console.log(i);
+      console.log(i + ': ' + ' Je suis un multiple de 7');
     }
   }
 }
